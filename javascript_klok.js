19354a81daefd991ae7da46adf49e9dac43431fd
Jane-Kolf <Jane-Kolf@users.noreply.github.com>
2021-03-20 20:14:20 +0100

    Challenge 2 Klok definitief
    Definitieve versie van de klok opdracht bij XT2

--- javascript_klok.js
+++ javascript_klok.js
@@ -1,24 +1,105 @@
 
-var today = new Date();
+/**
+ * Start clock
+ */
+ function initClock() {
 
-var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-document.getElementById('date').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()] + ' ' + today.getFullYear();
+	// init
+	renderClock();
 
+	// run every second
+	setInterval(renderClock, 1000);
+}
 
-var minutes = today.getMinutes;
+/**
+ * Render clock
+ */
+function renderClock() {
 
-document.getElementById('time').innerHTML = today.getHours() + ':' + today.getMinutes();
+	// get HTML elements
+	var clock = document.getElementById('time');
+	var date  = document.getElementById('date');
 
+	// time logic
+	var time  = getCurrentTime(new Date());
+	var sep   = flashSeperator(time['seconds']);
 
-var hour = today.getHours();
+	// display date
+	date.innerHTML = time['dayMonth'] + ' ' +  getCurrentMonth(time['month'])+ ' ' + time['Year'];
 
+	// display time
+	clock.innerHTML = time['hours'] + sep +  time['minutes'];
+}
 
+/**
+ * Flash seperator 
+ * @param integer - seconds
+ * @return string
+ */
+function flashSeperator(seconds) {
+	var sepClass = '';
+
+	// toggle class
+	if (seconds % 2 === 1) {
+		sepClass = ' class="trans"';
+	}
+
+	return '<span' + sepClass + '">:</span>';
+}
+
+/**
+ * Parse the time
+ * @param date object - current time 
+ * @return date array
+ */
+function getCurrentTime(date) {
+	var time = [];
+
+	// fill array
+	time['seconds'] = date.getSeconds();
+	time['minutes'] = date.getMinutes(),
+	time['hours']   = date.getHours();
+	time['month']   = date.getMonth();
+	time['day']     = date.getDay();
+  time['Year']    = date.getFullYear();
+  time['dayMonth']    = date.getDate();
+
+	// hours: add leading zero
+	if (time['hours'] < 10) {
+		time['hours'] = '0' + time['hours'];
+	}
+
+	// minutes: add leading zero
+	if (time['minutes'] < 10) {
+		time['minutes'] = '0' + time['minutes'];
+	}
+
+	return time;
+}
+
+/**
+ * Get current Month
+ * @param integer - month number
+ * @return string
+ */
+function getCurrentMonth(monthNumber) {
+	var months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+
+	return months[monthNumber];
+}
+
+// start
+initClock();
+
+var time  = new Date();
+var hour = time.getHours();
 
 if ( hour >= 6 && hour < 12){
 	document.getElementById("groet").innerHTML = "Goedemorgen mensen!";
 	document.getElementById("icon").innerHTML = "⏰";
 	document.body.style.backgroundImage = "url('ochtend_bg.jpg')";
 	document.getElementById("weer").src = "sun.png";
+
   }
   else if ( hour >= 12 && hour < 18){
 	document.getElementById("groet").innerHTML = "Goedemiddag mensen!";
